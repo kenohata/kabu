ee6ab1ad2f88144e2762dad58a516f9ef9dafdea
Ken Ohata <ohata@daraf.co.jp>
2014-12-10 22:30:39 +0900

    update user_spec.rb
    fix user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context :new_record? do
+    before do
+      @user = User.new
+    end
+
+    it { expect(@user.valid?).to eq false }
+
+    it do
+      @user.email = "ohata@daraf.co.jp"
+      @user.password = "password"
+      @user.password_confirmation = "password"
+
+      expect(@user.valid?).to eq true
+    end
+  end
 end
