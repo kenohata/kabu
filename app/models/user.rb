db2c78b2707629f58c72a967b26d7598253fc2c2
Ken Ohata <ohata@daraf.co.jp>
2014-12-10 22:32:33 +0900

    implement User
    implement User
    
    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, presence: true, length: { in: 6..16 }, if: :password
 end
