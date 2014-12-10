29b4e0d2afd15104a4df2c44d47c2998a4ede8fe
Ken Ohata <ohata@daraf.co.jp>
2014-12-10 22:57:47 +0900

    update registrations_controller_spec.rb

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe RegistrationsController, :type => :controller do
     end
   end
 
+  describe "POST create" do
+    it "returns http success" do
+      post :create, { user: { email: "ohata@daraf.co.jp", password: "password", password_confirmation: "password" } }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http failure" do
+      post :create, { user: { email: "ohata@daraf.co.jp", password: "password", password_confirmation: "passwrod" } }
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
 end
