6e7252fc096a66971aa6adf3aa25296d9aa7e2de
Ken Ohata <ohata@daraf.co.jp>
2014-12-10 22:58:24 +0900

    implement registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,20 @@
 class RegistrationsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(params_user)
+
+    if @user.save
+      render :new
+    else
+      render :new, status: 400
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
